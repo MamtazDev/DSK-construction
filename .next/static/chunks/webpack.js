302023df7497378e786596e19449bdaf9b8ad4de
tonni-akter-dev <tanniakhter123@gmail.com>
2023-11-02 10:13:35 +0600

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "84e1461103f730b9"; }
+/******/ 		__webpack_require__.h = function() { return "0480375acbd59d11"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
